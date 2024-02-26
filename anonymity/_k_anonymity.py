a247433bfce269c4df20b789511b836526a54523
Judith Sáinz-Pardo Díaz <sainzpardo@ifca.unican.es>
2024-02-26 16:35:12 +0100

    Add k-anonymity function and utils

---
+++ anonymity/_k_anonymity.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2024 Spanish National Research Council (CSIC)
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import typing
+import numpy as np
+import pandas as pd
+import pycanon
+from pycanon import anonymity
+from anonymity.utils import utils
+from copy import copy
+
+
+def k_anonymity(
+    data: pd.DataFrame,
+    ident: typing.Union[typing.List, np.ndarray],
+    quasi_ident: typing.Union[typing.List, np.ndarray],
+    k: int,
+    supp_level: float,
+    hierarchies: dict,
+) -> pd.DataFrame:
+    """Anonymize a dataset using k-anonymity.
+
+    :param data: data under study.
+    :type data: pandas dataframe
+
+    :param ident: list with the name of the columns of the dataframe
+        that are identifiers.
+    :type ident: list of strings
+
+    :param quasi_ident: list with the name of the columns of the dataframe
+        that are quasi-identifiers.
+    :type quasi_ident: list of strings
+
+    :param k: desired level of k-anonymity.
+    :type k: int
+
+    :param supp_level: maximum level of record suppression allowed
+        (from 0 to 100).
+    :type supp_level: float
+
+    :param hierarchies: hierarchies for generalizing the QI.
+    :type hierarchies: dictionary with a pandas dataframe for each QI
+
+    :return: anonymized data.
+    :rtype: pandas dataframe
+    """
+    data = utils.suppress_identifiers(data, ident)
+
+    # Initially the level of generalization is 0 for all the QI
+    gen_level = {}
+    for qi in quasi_ident:
+        gen_level[qi] = 0
+
+    k_real = pycanon.anonymity.k_anonymity(data, quasi_ident)
+    quasi_ident_gen = copy(quasi_ident)
+
+    if k_real >= k:
+        print(f"The data verifies k-anonymity with k={k_real}")
+        return data
+
+    while k_real < k:
+        equiv_class = pycanon.anonymity.utils.aux_anonymity.get_equiv_class(
+            data, quasi_ident
+        )
+        len_ec = [len(ec) for ec in equiv_class]
+        if k > max(len_ec):
+            print(
+                f"For the given data, k-anonymity with "
+                f"k={k} cannot be achieved only by suppression"
+            )
+        else:
+            data_ec = pd.DataFrame({"equiv_class": equiv_class, "k": len_ec})
+            data_ec_k = data_ec[data_ec.k < k]
+            records_sup = sum(data_ec_k.k.values)
+            if records_sup * 100 / len(data) <= supp_level:
+                ec_elim = np.concatenate(
+                    [
+                        pycanon.anonymity.utils.aux_functions.convert(ec)
+                        for ec in data_ec_k.equiv_class.values
+                    ]
+                )
+                anonim_data = data.drop(ec_elim).reset_index()
+                assert pycanon.anonymity.k_anonymity(anonim_data, quasi_ident) >= k
+                return anonim_data
+
+        if len(quasi_ident_gen) == 0:
+            print(f"The anonymization cannot be carried out for the given value k={k}")
+            return data
+
+        # Get the attribute with more unique values
+        qi_gen = quasi_ident_gen[
+            np.argmax([len(np.unique(data[qi])) for qi in quasi_ident_gen])
+        ]
+
+        generalization_qi = utils.apply_hierarchy(
+            data[qi_gen].values, hierarchies[qi_gen], gen_level[qi_gen] + 1
+        )
+
+        if generalization_qi is None:
+            if qi_gen in quasi_ident_gen:
+                quasi_ident_gen.remove(qi_gen)
+        else:
+            data[qi_gen] = generalization_qi
+            gen_level[qi_gen] = gen_level[qi_gen] + 1
+
+        k_real = pycanon.anonymity.k_anonymity(data, quasi_ident)
+    return data
---
+++ anonymity/utils/utils.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2024 Spanish National Research Council (CSIC)
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import typing
+import numpy as np
+import pandas as pd
+
+
+def suppress_identifiers(
+    data: pd.DataFrame, ident: typing.Union[typing.List, np.ndarray]
+):
+    for i in ident:
+        data[i] = ["*"] * len(data)
+    return data
+
+
+def apply_hierarchy(data: list, hierarchies: dict, level: int):
+    num_level = len(hierarchies.keys()) - 1
+    if level > num_level:
+        return None  # Change to exception
+    pos = []
+    for elem in data:
+        pos.append(np.where(hierarchies[level - 1].values == elem)[0][0])
+    data_anon = hierarchies[level].values[pos]
+    return data_anon
