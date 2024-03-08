224a53add01026b4ff3aebffbcd4bf4034ace2ba
Judith Sáinz-Pardo Díaz <sainzpardo@ifca.unican.es>
2024-03-08 10:49:21 +0100

    Update theme

--- docs/conf.py
+++ docs/conf.py
@@ -41,5 +41,5 @@ source_suffix = [".rst", ".md"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'classic'
+html_theme = 'press'
 html_static_path = ['_static']
