ed06f0e4ce603bc01f1332d3cfdfa85b98bb5efa
Judith Sáinz-Pardo Díaz <sainzpardo@ifca.unican.es>
2024-08-14 13:06:40 +0200

    feat: move to version 1.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath("./../../"))
 project = "ANJANA"
 copyright = "2024, Spanish National Research Council (CSIC)"
 author = "Judith Sáinz-Pardo Díaz (CSIC)"
-release = "0.2.2"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
